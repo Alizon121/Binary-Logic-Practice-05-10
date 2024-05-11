68df587b4a4c6ea9e1e4a6090c24b85f0b409ba1
Andrew <andrew.lizon12@gmail.com>
2024-05-10 18:05:19 -0700

    Completed 3-convertToBase16 module

--- 1-base-conversions/3-convertToBase16.js
+++ 1-base-conversions/3-convertToBase16.js
@@ -3,7 +3,12 @@
 /******************************************************************************/
 
 const convertToBase16 = element => {
-  // Your code here 
+  if (typeof element === 'string') {
+    let base10 = parseInt(element.slice(2), 2)
+    return '0x' + base10.toString(16)
+  } else {
+    return '0x' + element.toString(16)
+  }
 };
 
 /******************************************************************************/
